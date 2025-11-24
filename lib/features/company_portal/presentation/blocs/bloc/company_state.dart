10e1f6476a5a4e1ee8947be238fe5a6249ffcf3b
Abbas <abbas.h.8215@gmail.com>
2025-11-24 19:33:26 +0300

    adding

--- lib/features/company_portal/presentation/blocs/bloc/company_state.dart
+++ lib/features/company_portal/presentation/blocs/bloc/company_state.dart
@@ -1,6 +1,25 @@
 part of 'company_bloc.dart';
 
-@freezed
-class CompanyState with _$CompanyState {
-  const factory CompanyState.initial() = _Initial;
+@immutable
+abstract class CompanyState {}
+
+class CompanyInitial extends CompanyState {}
+
+class CompanyLoading extends CompanyState {}
+
+class CompanyLoaded extends CompanyState {
+  final CompanyEntity company;
+  CompanyLoaded(this.company);
 }
+
+class CandidateResults extends CompanyState {
+  final List<Map<String, dynamic>> candidates;
+  CandidateResults(this.candidates);
+}
+
+class CompanyError extends CompanyState {
+  final String message;
+  CompanyError(this.message);
+}
+
+class BookmarkAdded extends CompanyState {}
